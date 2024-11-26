8fdddde5ecf8774b9a2c454cb734baae32aaaf08
Thaw Maung Oo <thawmaungoo281103@gmail.com>
2024-11-26 16:26:34 +0630

    session configration finished

---
+++ app/sessionconfig.php
@@ -0,0 +1,41 @@
+<?php
+
+session_start();
+
+function setsession($key,$val){
+    $_SESSION[$key] = $val;
+}
+
+function getsession($key){
+    return $_SESSION[$key];
+}
+
+function verifysession($key){
+    return issest($_SESSION[$key]);
+}
+
+function unsetsession($key){
+    unset($_SESSION[$key]);
+}
+
+function destroyallsession(){
+    session_destroy();
+}
+
+function authfailed(){
+    if(!isset($_SESSION['email']) && !isset($_SESSION['password'])){
+        return true;
+    }
+}
+
+function redirectto($url){
+    header("Location:$url");
+}
+
+function formatprint(array $array){
+    echo "<pre>".print_r($array,true)."</pre>";
+}
+
+
+
+?>
